4c915b89fc212fcc1a85f7946c21d94e86a49ccb
Kamil Kasjaniuk <multiprojekt.kk@gmail.com>
2021-12-18 19:58:00 +0100

    6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -7,15 +7,5 @@ public class TestingMain {
 
     public static void main(String[] args) {
 
-        if(Calculator.add(10,10) == 20){
-            System.out.println("Calculator.add() test OK");
-        }else{
-            System.out.println("Calculator.add() test Failed");
-        }
-        if(Calculator.subtract(30,10) == 20){
-            System.out.println("Calculator.subtract() test OK");
-        }else{
-            System.out.println("Calculator.subtract() test Failed");
-        }
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -3,11 +3,27 @@ package com.kodilla.testing.user;
 public class SimpleUser {
     private String username;
 
+    String realName;
+
     public String getUsername() {
         return username;
+    }
+
+    public SimpleUser(String username, String realName) {
+        this.username = username;
+        this.realName = realName;
     }
 
     public SimpleUser(String username) {
         this.username = username;
     }
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
 }
+
+

5d41fcfc074235a0fbe7c2e12b41f7c32575119d
Kamil Kasjaniuk <multiprojekt.kk@gmail.com>
2022-01-13 21:03:01 +0100

    6.5 Task Added Average and Median temperature calculation

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,6 +1,8 @@
 package com.kodilla.testing.weather.stub;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class WeatherForecast {
@@ -19,5 +21,25 @@ public class WeatherForecast {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
         }
         return resultMap;
+    }
+
+    public double calculateAverageTemperature(){
+        double averageTemperature = 0;
+        for(Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()){
+            averageTemperature += temperature.getValue();
+        }
+        return averageTemperature / temperatures.getTemperatures().size();
+    }
+
+    public double calculateMedianTemperature(){
+        List<Map.Entry<String, Double>> temperatureList = new LinkedList<>(temperatures.getTemperatures().entrySet());
+        temperatureList.sort(Map.Entry.comparingByValue());
+
+        if(temperatures.getTemperatures().size() % 2 == 1){
+            return temperatureList.get(temperatureList.size()/2).getValue();
+        }
+
+        return  ( temperatureList.get(temperatureList.size()/2).getValue() +
+                temperatureList.get(temperatureList.size()/2-1).getValue() ) /2;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.testing.weather.mock;
 import com.kodilla.testing.weather.stub.Temperatures;
 import com.kodilla.testing.weather.stub.WeatherForecast;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -20,6 +21,7 @@ class WeatherForecastTestSuite {
     private Temperatures temperaturesMock;
 
     @Test
+    @DisplayName("Forecast calculation")
     void testCalculateForecastWithMock() {
         //Given
         Map<String, Double> temperaturesMap = new HashMap<>();
@@ -36,5 +38,64 @@ class WeatherForecastTestSuite {
 
         //Then
         Assertions.assertEquals(5, quantityOfSensors);
+    }
+
+    @Test
+    @DisplayName("Average calculation")
+    void testCalculateAverageTemperature(){
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 0d);
+        temperaturesMap.put("Krakow", 1d);
+        temperaturesMap.put("Wroclaw", 1d);
+        temperaturesMap.put("Warszawa", 1d);
+        temperaturesMap.put("Gdansk", 2d);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double averageTemperature = weatherForecast.calculateAverageTemperature();
+
+        //Then
+        Assertions.assertEquals(1, averageTemperature);
+    }
+
+    @Test
+    @DisplayName("Median calculate when even List size")
+    void testCalculateMedianTemperatureWhenEven(){
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 0d);
+        temperaturesMap.put("Krakow", 1d);
+        temperaturesMap.put("Wroclaw", 2d);
+        temperaturesMap.put("Warszawa", 3d);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double medianTemperature = weatherForecast.calculateMedianTemperature();
+
+        //Then
+        Assertions.assertEquals(1.5, medianTemperature);
+    }
+
+    @Test
+    @DisplayName("Median calculate when odd List size")
+    void testCalculateMedianTemperatureWhenOdd(){
+        //Given
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 0d);
+        temperaturesMap.put("Krakow", 1d);
+        temperaturesMap.put("Wroclaw", 2d);
+        temperaturesMap.put("Warszawa", 3d);
+        temperaturesMap.put("Gdansk", 4d);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        double medianTemperature = weatherForecast.calculateMedianTemperature();
+
+        //Then
+        Assertions.assertEquals(2, medianTemperature);
     }
 }

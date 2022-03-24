649bb8863ec6644379c0e32c898e5e8d5975a96d
Kamil Kasjaniuk <multiprojekt.kk@gmail.com>
2022-03-24 21:29:23 +0100

    16.6.1

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -40,4 +40,29 @@ class DbManagerTestSuite {
         statement.close();
         assertEquals(6, counter);
     }
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "select u.FIRSTNAME,u.LASTNAME, COUNT(p.BODY) AS POSTS from users u\n" +
+                "join posts p on u.ID = p.USER_ID\n" +
+                "group by u.ID\n" +
+                "HAVING COUNT(p.BODY) > 1";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        assertEquals(2, counter);
+    }
 }

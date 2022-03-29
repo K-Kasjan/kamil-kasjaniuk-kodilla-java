8b8d69474e243ea86706a31e7a640aa86b046200
Kamil Kasjaniuk <multiprojekt.kk@gmail.com>
2022-03-29 21:18:32 +0200

    17.3

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,8 +1,11 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
 import com.sun.istack.NotNull;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
@@ -20,11 +23,27 @@ public class TaskList {
     @Column(name = "DESCRIPTION")
     private String description;
 
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Task> tasks = new ArrayList<>();
+
     public TaskList(){}
 
     public TaskList(String listName, String description) {
         this.listName = listName;
         this.description = description;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    private void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     public int getId() {
